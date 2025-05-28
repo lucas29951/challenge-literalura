38a93515e0a1b529c84cfae41ae2b40815b4e641
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-05-28 20:32:18 -0300

    Logica agregada de la clase principal

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,5 +1,6 @@
 package com.alura.literalura;
 
+import com.alura.literalura.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,9 @@ public class LiteraluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo!");
+
+		Principal app = new Principal();
+		app.menuDeOpciones();
+		
 	}
 }
