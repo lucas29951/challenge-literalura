2394e896380796a3c36522309c27a64ac5e7d79b
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-05-29 16:34:33 -0300

    Modificado

--- src/main/java/com/alura/literalura/service/ConvierteDatos.java
+++ src/main/java/com/alura/literalura/service/ConvierteDatos.java
@@ -7,7 +7,7 @@ public class ConvierteDatos implements IConvierteDatos {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    
+
     @Override
     public <T> T obtenerDatos(String json, Class<T> clase) {
         try {
