e8199e1f1ead4c296a172608ce76412955f40f0a
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-02 15:23:16 -0300

    Metodo agregado al repositorio de autores para buscar por nombre

--- src/main/java/com/alura/literalura/repository/AutorRepository.java
+++ src/main/java/com/alura/literalura/repository/AutorRepository.java
@@ -3,6 +3,9 @@ package com.alura.literalura.repository;
 import com.alura.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AutorRepository extends JpaRepository<Autor, Long> {
 
+    Optional<Autor> findByNombre(String nombre);
 }
