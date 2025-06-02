2b085505e6a677e840601c6f72aa2abce8302f72
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-01 21:54:16 -0300

    Archivo application del proyecto modificado - repositorio de libro agregado

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,12 +1,17 @@
 package com.alura.literalura;
 
 import com.alura.literalura.principal.Principal;
+import com.alura.literalura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private LibroRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
@@ -15,7 +20,7 @@ public class LiteraluraApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Principal app = new Principal();
+		Principal app = new Principal(repository);
 		app.menuDeOpciones();
 		
 	}
