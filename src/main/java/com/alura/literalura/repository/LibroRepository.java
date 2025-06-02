a8a1250c61726ec6a120bf0f31b4034d31018a78
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-02 16:14:53 -0300

    Metodos añadidos para listar idiomas y libros por idioma

--- src/main/java/com/alura/literalura/repository/LibroRepository.java
+++ src/main/java/com/alura/literalura/repository/LibroRepository.java
@@ -2,7 +2,9 @@ package com.alura.literalura.repository;
 
 import com.alura.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
@@ -10,4 +12,10 @@ public interface LibroRepository extends JpaRepository<Libro, Long> {
     Optional<Libro> findByTituloContainsIgnoreCase(String nombreLibro);
 
     Optional<Libro> findByTitulo(String titulo);
+
+    @Query(value = "SELECT DISTINCT(libros.idioma) FROM libros", nativeQuery = true)
+    List<String> mostrarTodosLosIdiomas();
+
+    @Query(value = "SELECT l FROM Libro l WHERE l.idioma ILIKE %:idiomaBuscado%")
+    List<Libro> mostrarLibrosPorIdioma(String idiomaBuscado);
 }
