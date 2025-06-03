424ae8568767a99ff82207d58793083d553b2a06
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-06-03 11:56:01 -0300

    Modificacion del metodo de autores vivos por año

--- src/main/java/com/alura/literalura/repository/AutorRepository.java
+++ src/main/java/com/alura/literalura/repository/AutorRepository.java
@@ -11,6 +11,6 @@ public interface AutorRepository extends JpaRepository<Autor, Long> {
 
     Optional<Autor> findByNombre(String nombre);
 
-    @Query(value = "SELECT a FROM Autor a WHERE a.fallecimiento >= :añoBuscado")
+    @Query(value = "SELECT a FROM Autor a WHERE a.fallecimiento > :añoBuscado AND a.nacimiento < :añoBuscado")
     List<Autor> autoresVivosHastaDeterminadoAño(int añoBuscado);
 }
